0ecae0dbdcfa964fd04a46d4c36c1a512702bbda
Oleg Urin <olegurin76@mail.ru>
2025-07-18 17:56:07 +0300

    добавил кнопку удаления задачи

--- task_manager/src/Entities/TaskItem/TaskItem.tsx
+++ task_manager/src/Entities/TaskItem/TaskItem.tsx
@@ -1,8 +1,11 @@
 import type { Task } from "../../shared/model/AboutTask"
 import { Avatar, Card } from 'antd';
-import { EditOutlined  } from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined  } from '@ant-design/icons';
 import { Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { useTasks } from "../../app/providers/TaskProvider/TaskProvider";
+import { useState } from "react";
+import {Modal} from "antd";
 import {Tag} from "antd";
 import {
   CheckCircleOutlined,
@@ -11,10 +14,24 @@ import {
 } from '@ant-design/icons';
 
 const TaskItem: React.FC<{task: Task}> = ({task}) => {
-
+    const { deleteTask } = useTasks();
     const navigate = useNavigate();
 
     const { Meta } = Card;
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
     const getPriorityColor = () => {
     switch (task.priority) {
@@ -41,17 +58,40 @@ const TaskItem: React.FC<{task: Task}> = ({task}) => {
     }
   };
 
+  const handleDelete = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    deleteTask(task.id);
+    handleOk();
+  };
+  
+
     return(
+      <>
+      <Modal
+        title="Подтверждение удаления"
+        closable={{ 'aria-label': 'Custom Close Button' }}
+        open={isModalOpen}
+        onOk={handleDelete}
+        onCancel={handleCancel}
+      >
+        <p>Вы действительно хотите удалить задачу ?</p>
+      </Modal>
         <Card style={{ width: 320 }}
             title={task.title}
-            extra={
+            extra={[
                 <Button
                     type="text"
                     icon={<EditOutlined />}
                     onClick={() => navigate(`/task/${task.id}`)}
-                />
-                
-            }
+                />,
+                <Button
+                  key="delete"
+                  type="text"
+                  danger
+                  icon={<DeleteOutlined />}
+                  onClick={showModal}
+                />,
+            ]}
             actions={[<Tag color="default">{task.category}</Tag>,
             <Tag icon={getStatusIcon()} color={getStatusColor()}>{task.status}</Tag>,
             <Tag color={getPriorityColor()}>{task.priority}</Tag>]}
@@ -62,7 +102,7 @@ const TaskItem: React.FC<{task: Task}> = ({task}) => {
                 description={task.description}
             />
         </Card>
-        
+      </>
     )
 }
 
