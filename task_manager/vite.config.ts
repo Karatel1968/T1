24cfb1965fe94427506e208aa5694c64d44713e6
Oleg Urin <olegurin76@mail.ru>
2025-07-20 23:58:20 +0300

    собрал проект

--- task_manager/vite.config.ts
+++ task_manager/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/T1/',
+  base: '/T1/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
