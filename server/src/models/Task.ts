d6947f55140a1d870d60edc5d33699baf176e062
Oleg Urin <olegurin76@mail.ru>
2025-07-24 01:02:40 +0300

    добавил комментарии в формате JSDoc к интерфейсам компонентов

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -1,3 +1,16 @@
+/**
+ * Основной интерфейс задачи
+ * @interface
+ * @property {string} id - Автогенерируемый UUID (не редактируется клиентом)
+ * @property {string} title - Обязательное поле (валидация: 3-100 символов)
+ * @property {string} description - Опциональное поле
+ * @property {TaskStatus} status - Значение по умолчанию: 'todo'
+ * @property {TaskPriority} priority - Влияет на сортировку в API
+ * @property {TaskCategory} category - Используется для фильтрации
+ * @property {string} createdAt - ISO-дата (автозаполнение)
+ * @property {string} updatedAt - ISO-дата (автообновление при изменениях)
+ */
+
 export interface Task {
   id: string;
   title: string;
--- task_manager/src/shared/model/AboutTask.ts
+++ task_manager/src/shared/model/AboutTask.ts
@@ -1,3 +1,8 @@
+/**
+ * Категории задач для классификации
+ * @enum {string}
+ */
+
 export enum TaskCategory {
   Bug = 'Bug',
   Feature = 'Feature',
@@ -6,18 +11,39 @@ export enum TaskCategory {
   Test = 'Test',
 }
 
+/**
+ * Статусы задачи для системы управления задачами
+ * @enum {string}
+ */
 export enum TaskStatus {
   Todo = 'To Do',
   InProgress = 'In Progress',
   Done = 'Done',
 }
 
+/**
+ * Приоритеты выполнения задачи
+ * @enum {string}
+ */
 export enum TaskPriority {
   Low = 'Low',
   Medium = 'Medium',
   High = 'High',
 }
 
+
+/**
+ * Основной интерфейс задачи
+ * @interface
+ * @property {string} id - Автогенерируемый UUID (не редактируется клиентом)
+ * @property {string} title - Обязательное поле (валидация: 3-100 символов)
+ * @property {string} description - Опциональное поле
+ * @property {TaskStatus} status - Значение по умолчанию: 'todo'
+ * @property {TaskPriority} priority - Влияет на сортировку в API
+ * @property {TaskCategory} category - Используется для фильтрации
+ * @property {string} createdAt - ISO-дата (автозаполнение)
+ * @property {string} updatedAt - ISO-дата (автообновление при изменениях)
+ */
 export interface Task {
   id: string;
   title: string;
